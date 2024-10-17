85fe78d53a34de204080e8870b2db6d87739b536
Ansar Ahmad <mohammad.a.ahmad@icloud.com>
2024-10-17 19:23:42 +0100

    design changes

--- KennysCuts/Context/DatabaseSeeder.cs
+++ KennysCuts/Context/DatabaseSeeder.cs
@@ -36,7 +36,7 @@ namespace KennysCuts.Context
                 await _roleManager.CreateAsync(new IdentityRole("Customers"));
 
                 var adminEmail = "admin@barbers.com";
-                var adminPassword = "KennyTheBarber123!";
+                var adminPassword = "Kenny11!";
 
                 var admin = new User
                 {
