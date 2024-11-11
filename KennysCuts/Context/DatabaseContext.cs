b7b34dcc69a054351a0a2c0aa92748dea98ab111
Ansar Ahmad <mohammad.a.ahmad@icloud.com>
2024-11-11 12:45:31 +0000

    another set of changes

--- KennysCuts/Context/DatabaseContext.cs
+++ KennysCuts/Context/DatabaseContext.cs
@@ -6,15 +6,25 @@ namespace KennysCuts.Context
 {
     public class DatabaseContext : IdentityDbContext<User>
     {
+        private IWebHostEnvironment _environment;
         public DbSet<Services> Services { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Barber> Barber { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, IWebHostEnvironment enviornment) : base(options)
+        {
+            _environment = enviornment;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
         {
-            var folder = Environment.SpecialFolder.MyDocuments;
-            var path = Environment.GetFolderPath(folder);
-            var dbPath = Path.Join(path, "database.db");
-            optionbuilder.UseSqlite($"Data Source={dbPath}");
+            var folder = Path.Combine(_environment.WebRootPath, "database");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+
+            optionbuilder.UseSqlite($"Data Source={folder}/KennysCuts.db");
         }
     }
 }
